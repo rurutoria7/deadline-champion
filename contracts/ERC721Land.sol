490c7b068b16c74ed892fefa529da26e0b9309ef
rurutoria7 <nandokabeam@gmail.com>
2022-06-04 23:38:45 +0800

    first commit

---
+++ contracts/ERC721Land.sol
@@ -0,0 +1,99 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "../node_modules/@openzeppelin/contracts/token/ERC721/presets/ERC721PresetMinterPauserAutoId.sol";
+import "hardhat/console.sol";
+
+contract ERC721LAND is ERC721PresetMinterPauserAutoId {
+    using Counters for Counters.Counter;    
+    Counters.Counter private _tokenIdTracker;
+
+    struct Land
+    {
+        int minX;
+        int maxX;
+        int minY;
+        int maxY;
+    } constructor( string memory name, string memory symbol, string memory baseTokenURI
+    ) ERC721PresetMinterPauserAutoId(name, symbol, baseTokenURI){}
+
+    function _area(Land memory land) pure internal returns (int) { return (land.maxX-land.minX)*(land.maxY-land.minY); }
+    function max(int a, int b) internal pure returns (int) {return a >= b ? a : b;}
+    function min(int a, int b) internal pure returns (int) {return a >= b ? a : b;}
+
+
+    //Mapping from tokenId address to a land
+    mapping(uint256 => Land) landOfToken;
+    
+    //requre: send is owner
+    //split token to two, setTokenURI for original one, create new one
+    //send new one to sender
+
+    function mintReturnTokenId(address to) public virtual returns (uint256) {
+        console.log("in mintReturnTokenId");
+        require(hasRole(MINTER_ROLE, _msgSender()), "ERC721Land: must have minter role to mint");
+
+        // We cannot just use balanceOf to create the new tokenId because tokens
+        // can be burned (destroyed), so we need a separate counter.
+        _mint(to, _tokenIdTracker.current());
+        uint256 createdTokenId = _tokenIdTracker.current();
+        _tokenIdTracker.increment();
+        return createdTokenId;
+    }
+
+    function splitToken(uint256 tokenId, int splitX, int splitY) public virtual {
+        require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721Land: caller is not owner nor approved");
+        _burn(tokenId);
+
+        //split to 4
+        Land storage original = landOfToken[tokenId];
+        Land[4] memory res = [Land(original.minX,splitX,original.minY,splitY)
+                            , Land(splitX,original.maxX,original.minY,splitY)
+                            , Land(original.minX,splitX,splitY,original.maxY)
+                            , Land(splitX,original.maxX,splitY,original.maxY)];
+        //if has area, mint, setLand, send to sender
+        for (uint256 i=0; i<4; i++){
+            if (_area(res[i]) > 0){
+                uint256 createdTokenId = mintReturnTokenId(_msgSender());
+                setTokenLand(createdTokenId, res[i]);
+            }
+        }
+    }
+
+    function mergeToken(uint256 tokenIdA, uint256 tokenIdB) public virtual {
+        require(_isApprovedOrOwner(_msgSender(), tokenIdA), "ERC721Land: caller is not owner nor approved");
+        require(_isApprovedOrOwner(_msgSender(), tokenIdB), "ERC721Land: caller is not owner nor approved");
+
+        Land memory lA = landOfToken[tokenIdA];
+        Land memory lB = landOfToken[tokenIdB];
+        Land memory res;
+
+        //merge horizontal
+        if (lA.minY==lB.minY && lA.maxY==lB.maxY && (lA.maxX==lB.minX || lA.minX==lB.maxX)) {
+            res = Land(min(lA.minX,lB.minX), max(lA.maxX,lB.maxX), lA.minY, lA.maxY);
+        }
+        //merge vertical
+        else if (lA.minX==lB.minX && lA.maxX==lB.maxX && (lA.maxY==lB.minY || lA.minY==lB.maxY)) {
+            res = Land(lA.minX, lA.maxX, min(lA.minY,lB.minY), max(lA.maxY,lB.maxY));
+        }
+        //cannot merge to rectangle
+        else{
+            require(false, "ERC721Land: targets shoulde be rectangle land after merged");
+        }
+
+        //burn tokenB
+        //set tokenIdA's Land to res
+        burn(tokenIdB);
+        setTokenLand(tokenIdA, res);
+    }
+
+    function setTokenLand(uint256 tokenId, Land memory land) public virtual {
+        require(hasRole(MINTER_ROLE, _msgSender()), "ERC721PresetMinterPauserAutoId: must have minter role to mint");
+        landOfToken[tokenId] = land;
+    }
+
+    function queryTokenLand(uint256 tokenId) public virtual returns (Land memory){
+        return landOfToken[tokenId];
+    }
+}
