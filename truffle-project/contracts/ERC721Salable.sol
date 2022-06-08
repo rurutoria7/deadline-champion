5bcf130e030d10ab191e33e5afe5950c3ff46e01
rurutoria7 <nandokabeam@gmail.com>
2022-06-08 14:40:51 +0800

    piyan

--- truffle-project/contracts/ERC721Salable.sol
+++ truffle-project/contracts/ERC721Salable.sol
@@ -8,17 +8,33 @@ import "../node_modules/@openzeppelin/contracts/token/ERC721/presets/ERC721Prese
 abstract contract ERC721Salable is ERC721PresetMinterPauserAutoId {
 
     ERC20PresetMinterPauser erc20;
+    mapping(uint256 => bool) forSale;
+    mapping(uint256 => uint256) price;
+    mapping(uint256 => address) salerOf;
 
     constructor(ERC20PresetMinterPauser _erc20) {
         erc20 = _erc20;
     }
 
-    function sale(uint256 tokenId, uint256 price) public {
+    function sale(uint256 tokenId, uint256 _price) public {
+        require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721Salable: transfer caller is not owner nor approved");
+        require(_exists(tokenId), "ERC721Salable: URI query for nonexistent token");
 
+        forSale[tokenId] = true;   
+        price[tokenId] = _price;
+        salerOf[tokenId] = _msgSender();
     }
 
-    function sale(uint256 tokenId, uint256 price) public {
-        
+    function unSale(uint256 tokenId) public {
+        forSale[tokenId] = false;
+        transferFrom(address(this), salerOf[tokenId], tokenId);
+    }
+
+    function buy(uint256 tokenId, uint256 price) public {
+        require(forSale[tokenId], "ERC721Salable: this token is not salable");
+        forSale[tokenId] = false;
+        erc20.transfer(price[tokenId], salerOf[tokenId]);
+        transferFrom(address(this), msg.sender, tokenId);
     }
 
 }
